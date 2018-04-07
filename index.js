f07d289cffe42827632ab27cb907f49308d68c79
alxpez <alexperezdev@gmail.com>
2018-04-07 12:14:06 +1200

    fix(delay)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
 const http = require('http').createServer().listen(8000)
 const octokit = require('@octokit/rest')()
+
+require('dotenv').config()
 const updateInfo = {
   email: process.env.EMAIL,
   owner: process.env.OWNER,
@@ -12,7 +13,7 @@ updateDate(updateInfo)
 
 async function updateDate ({email, owner, repo, token}) {
   const date = new Date()
-  const delay = (((24 - date.getUTCHours()) * 60) - (60 - date.getUTCMinutes())) * 60000
+  const delay = (((24 - date.getUTCHours()) * 60) - date.getUTCMinutes()) * 60000
 
   const display = ':spiral_calendar: ' + date.toUTCString().slice(0, date.toUTCString().lastIndexOf(' ') - 9) + ' (UTC)'
 
@@ -22,7 +23,7 @@ async function updateDate ({email, owner, repo, token}) {
 
   try {
     octokit.authenticate({type: 'oauth', token})
-    
+
     const readme = await octokit.repos.getReadme({owner, repo})
     await octokit.repos.updateFile({owner, repo, message, content, committer, sha: readme.data.sha, path: readme.data.path})
     await octokit.repos.edit({owner, name: repo, repo, description: display})
