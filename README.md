1fca44b10508be47d52572da2ac0a38a2b799b6d
alxpez <alexperezdev@gmail.com>
2018-04-16 22:32:43 +1200

    update to new functions

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# today-is-worker
+# daily-worker ⚙️
 
-Its sole function is to update **today-is** repository (README.md and repoDescription) with a new UTC date, every 24 hours.
+> I only work once every 24h
 
-> **alxpez/today-is** an embedded calendar within my github pinned repositories.
+### Current tasks
+- Update **today-is** with a new UTC date
+- Update **quote42day** with a new quote
+
+> **alxpez/today-is** embedded calendar as pinned repository
+
+> **alxpez/quote42day** a little daily inspiration
--- index.js
+++ index.js
@@ -1,37 +1,94 @@
-const http = require('http').createServer().listen(8000)
+require('dotenv').config()
+
+const http = require('http')
+const axios = require('axios')
 const octokit = require('@octokit/rest')()
 
-require('dotenv').config()
-const updateInfo = {
-  email: process.env.EMAIL,
-  owner: process.env.OWNER,
-  repo: process.env.REPO,
-  token: process.env.TOKEN
-}
 
-updateDate(updateInfo)
+// Necessary server for NOW deployment
+http.createServer().listen(8000)
 
-async function updateDate ({email, owner, repo, token}) {
+// Work initializer
+dailyTasks()
+
+
+// Tasks index
+async function dailyTasks () {
   const date = new Date()
   const delay = (((24 - date.getUTCHours()) * 60) - date.getUTCMinutes()) * 60000
 
-  const display = ':spiral_calendar: ' + date.toUTCString().slice(0, date.toUTCString().lastIndexOf(' ') - 9) + ' (UTC)'
+  await updateDate(date)
+  await updateQuote()
 
-  const content = Buffer.from('# ' + display).toString('base64')
-  const message = 'update date'
-  const committer = {name: owner, email}
+  setTimeout(dailyTasks, delay)
+  console.log(`${delay} milliseconds till next update`)
+}
+
+
+// Updates today-is repo
+async function updateDate (date) {
+  const decription = `:spiral_calendar: ${date.toUTCString().slice(0, date.toUTCString().lastIndexOf(' ') - 9)} (UTC)`
+
+  await updateRepo('today-is', decription, content, 'calendar auto-update: by daily-worker')
+  console.log(`Today is: ${description}`)
+}
+// Updates quote42day repo
+async function updateQuote () {
+  const quote = await getRandomQuote()
+
+  const decription = `:scroll: by ${quote.author}`
+  const content = Buffer.from(`> ${quote.quote}
+  ## ${quote.author} [:scroll:](${quote.permalink})`).toString('base64')
+
+  await updateRepo('quote42day', decription, content, 'quote auto-update: by daily-worker')
+  console.log(`"${quote.quote}" [${quote.author}]`)
+}
+
+
+// --- APIs methods --- //
+
+
+const ghInfo = {
+  email: process.env.EMAIL,
+  owner: process.env.OWNER,
+  token: process.env.TOKEN
+}
+
+async function updateRepo(repo, description, content, message) {
+  const committer = {name: ghInfo.owner, email: ghInfo.email}
 
   try {
-    octokit.authenticate({type: 'oauth', token})
+    await octokit.authenticate({type: 'oauth', token: ghInfo.token})
 
-    const readme = await octokit.repos.getReadme({owner, repo})
-    await octokit.repos.updateFile({owner, repo, message, content, committer, sha: readme.data.sha, path: readme.data.path})
-    await octokit.repos.edit({owner, name: repo, repo, description: display})
+    // Updates the repo description, if not null
+    if (description)
+      octokit.repos.edit({owner: ghInfo.owner, name: repo, repo, description})
+
+    const readme = await octokit.repos.getReadme({owner: ghInfo.owner, repo: repo})
+    octokit.repos.updateFile({
+      owner: ghInfo.owner, repo, message, content, committer,
+      sha: readme.data.sha, path: readme.data.path
+    })
   } catch (e) {
     console.error('IT BROKE! -> ' + e)
     process.exit(1)
   }
+}
 
-  setTimeout(updateDate, delay, updateInfo)
-  console.log(delay + ' milliseconds till next update');
+
+// This allows to add new sources (with same API resp format) and randomize them
+const QT_SRC = [
+  'https://talaikis.com/api/quotes/random/',
+  'http://quotes.stormconsultancy.co.uk/random.json'
+]
+
+async function getRandomQuote() {
+  let srcIndex = Math.floor(Math.random() * Math.floor(QT_SRC.length))
+
+  try {
+    const resp = await axios.get(QT_SRC[srcIndex])
+    return resp.data
+  } catch (error) {
+    console.error(error);
+  }
 }
